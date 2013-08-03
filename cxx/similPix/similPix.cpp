3a6d166ed5ac61a93fed9a7396aa697f1c0ee366
matus <matus@quad.com>
2013-08-03 14:34:14 +0200

    plus plus

--- cxx/similPix/similPix.cpp
+++ cxx/similPix/similPix.cpp
@@ -62,10 +62,10 @@ int main(int argc, char** argv){
 	const unsigned int Fws=F/2; // F window size
 	
 	int Ddim[]={P*P*R*F,P*R*F,R*F,F};
-	double* S = new double[N1*N2*R*(F-Fws)*(F-Fws)*2];
-	const unsigned int shape[] = {N1,N2,R*4,F-Fws,F-Fws,2};
+	double* S = new double[N1*N2*4*R*2];
+	const unsigned int shape[] = {N1,N2,R*4,2};
 	
-	int Sdim[]={N2*4*R*F*(F-Fws)*2,4*R*F*(F-Fws)*2,pow(F-Fws,2)*2,(F-Fws)*2,2};
+	int Sdim[]={N2*4*R*2,4*R*2,2};
 	
 	//printf("D1[0]= %u", D1[0]);
 	//printf("D1[1]= %u", D1[234*m1+12*m2+12*m3+0*m4+45]);
@@ -80,27 +80,27 @@ int main(int argc, char** argv){
 	printf("n1=%d\n",n1);
 	//if (n1 % 100==0 && n1>0) { sprintf(buffer,"S%02d.npy",n1/100);
 	//	cnpy::npy_save(buffer,S,shape,5,"w");}
-	for (int n2=0;n2<10;n2++){
+	for (int n2=0;n2<N2;n2++){
 		printf("n2=%d\n",n2);
 	for (int r1=0;r1<R;r1=r1+1){
-	for (int f1=0;f1<F-Fws;f1=f1+1){
+	//for (int f1=0;f1<F-Fws;f1=f1+1){
 	for (int m1=0;m1<2;m1++){
 	for (int ori=0;ori<4;ori++){
-	for (int f2=0;f2<F-Fws;f2=f2+1){
+	//for (int f2=0;f2<F-Fws;f2=f2+1){
 		//res1=0;res2=0;
-		index1= n1*Sdim[0]+n2*Sdim[1]+(ori*5+r1)*Sdim[2]+f1*Sdim[3]+f2*Sdim[4]+m1;
+		index1= n1*Sdim[0]+n2*Sdim[1]+(ori*R+r1)*Sdim[2]+m1;
 		S[index1]=0;
 	for (int x=0;x<P;x=x+1){
 	for (int y=0;y<P;y=y+1){
-	for (int f=0;f<Fws;f++){
+	for (int f=F/4;f<3*F/4;f++){
 		// index rotation
 		if (m1==1) ym=P-y-1; else ym=y;
 		if (ori==0) {i=x;j=ym;}
 		if (ori==1){i=ym;j=P-x-1;}
 		if (ori==2){i=P-x-1;j=P-ym-1;}
 		if (ori==3){i=P-ym-1;j=x;}
-		temp= D1[n1*Ddim[0]+i*Ddim[1]+j*Ddim[2]+r1*Ddim[3]+f+f1]
-			-D2[n2*Ddim[0]+x*Ddim[1]+y*Ddim[2]+r2*Ddim[3]+f+f2];
+		temp= D1[n1*Ddim[0]+i*Ddim[1]+j*Ddim[2]+r1*Ddim[3]+f]
+			-D2[n2*Ddim[0]+x*Ddim[1]+y*Ddim[2]+r2*Ddim[3]+f];
 		//res1=res1+temp*temp;
 		S[index1]=S[index1]+temp*temp;
 	}}}
@@ -115,9 +115,9 @@ int main(int argc, char** argv){
 	//	if (res2<S[a*N+b]) {S[a*N+b]=res2;S[a+b*N]=r+4;}
 	//}
 	//printf("res1= %f\n", sqrt(res1));
-	}}}}}}}
+	}}}}}//}}
 	//printf("S[0]= %f\n", S[0*N*N+0*N+3]);
 	sprintf(buffer,"S%03dx%03d.npy",atoi(argv[1]),atoi(argv[2]));
-    cnpy::npy_save(buffer,S,shape,6,"w");
+    cnpy::npy_save(buffer,S,shape,4,"w");
 	//printf("finished\n");
 }	
