corrected alternative orientation computation

code/Analysis.py
@@ -22,6 +22,60 @@ def initVP(vpl=1,evl=1):
     if event>=0: sw=-400; ew=400;# start and end (in ms) of the window
     else: sw=-800; ew=0
     fw=int(np.round((abs(sw)+ew)/1000.0*hz))
+
+##########################################
+#
+#       compute rotation angles
+#
+##########################################
+
+def computePhi(path,PLOT=False):
+    f=open(path+'purxy.pickle','rb')
+    purxy=pickle.load(f)
+    f.close()
+    phis=np.zeros(len(purxy))*np.nan
+    alp=0.9; f1lim=2
+    for i in range(1000):#len(purxy)):
+        pxy=np.reshape(purxy[i],(len(purxy[i])/2,2))
+        pxy-=pxy[pxy.shape[0]/2]
+        if PLOT:
+            plt.figure(1,figsize=(10,10));plt.subplot(2,2,vp)
+            plt.plot(pxy[:,0],pxy[:,1],'k',alpha=alp)
+            plt.grid(b=False);plt.title('centered trajectory')
+        temp=np.diff(pxy,axis=0)
+        if PLOT:
+            plt.figure(2,figsize=(10,10));plt.subplot(2,2,vp)
+            plt.plot(np.linalg.norm(temp,axis=1),'k',alpha=alp)
+            plt.grid(b=False);plt.title('velocity')
+        phi=np.arctan2(temp[:,1],temp[:,0])
+        
+        pold=np.copy(phi)
+        df=np.diff(phi)
+        df=-np.sign(df)*(np.abs(df)>np.pi)
+        phi[1:]+=np.cumsum(df)*2*np.pi
+        
+        assert np.all(np.diff(phi)<np.pi)
+        phis[i]=np.median(phi)
+        phi-=phis[i]
+
+        if PLOT:
+            plt.figure(3,figsize=(10,10));plt.subplot(2,2,vp)
+            plt.plot(np.linspace(0,phi.size,phi.size),
+                     phi/np.pi*180,'k',alpha=alp)
+            plt.grid(b=False)
+            plt.ylim([-60,60])
+            c=np.cos(phis[i])*f1lim;s=np.sin(phis[i])*f1lim
+            plt.figure(1,figsize=(10,10));plt.subplot(2,2,vp)
+            #plt.plot([-c,c],[-s,s],'g',alpha=alp)
+            plt.gca().set_aspect(1);plt.grid(b=False);
+            plt.xlim([-f1lim,f1lim]);plt.ylim([-f1lim,f1lim])
+            plt.figure(4,figsize=(10,10));plt.subplot(2,2,vp);
+            plt.grid(b=False)
+            plt.plot(np.linspace(0,phi.size,phi.size),
+                     np.sort(phi)/np.pi*180,'k',alpha=alp)
+    return phis
+
+
     
 
 def saveFigures(name):
@@ -29,10 +83,9 @@ def saveFigures(name):
         plt.figure(i)
         plt.savefig(figpath+'E%d'%event+name+'%02dvp%03d.png'%(i,vp))
 
-
         
-def plotSearchInfo(plot=True):
-    D=np.load(path+'si.npy')
+def plotSearchInfo(plot=True,suf=''):
+    si=np.load(path+'si.npy')
     if event<0:
         d=np.load(path+'finalev.npy')
         print 'finalev len:',d.shape
@@ -44,26 +97,33 @@ def plotSearchInfo(plot=True):
         sel=np.all(~np.isnan(d[:,1,:]),axis=1 )
         d=d[sel,:,:]
         print 'finalev len2:',d.shape
-        si=np.zeros((d.shape[0],D.shape[1]))*np.nan
+        si=np.zeros((d.shape[0],si.shape[1]))*np.nan
         si[:,[-2,-1]]=d[:,0,:2]
         si[:,[1,6,7]]=d[:,1,:]
         si[:,12]=np.inf # set to something greater than d[:,1,0]
         return si,[]
-    inds=D[D[:,14]>0,14]
-    phi=np.load(path+'phiTrack.npy')
-    phi=phi[inds==event,-1,1] # return median values
-    D=D[D[:,14]==event,:]
-    if event>0: assert phi.shape[0]==D.shape[0]
+    if suf=='3':
+        from Constants import OLPUR,CLPUR
+        phi=computePhi(path)
+        sel=np.logical_or(si[:,10]==OLPUR,si[:,10]==CLPUR)
+        sel=np.logical_and(si[:,14]>0,sel)
+        si=si[sel,:]
+        phi=phi[si[:,14]==event]
+    else: pass
+    
+    si=si[si[:,14]==event,:]
     if event==1:
-        # discard tracking events initiated by tracking or blink
-        sel=np.logical_and(~np.isnan(D[:,11]),D[:,11]-D[:,4]>=0)
-        D=D[sel,:];phi=phi[sel]
+        # discard tracking events initiated not by saccade (tracking or blink)
+        sel=np.logical_and(~np.isnan(si[:,11]),si[:,11]-si[:,4]>=0)
+        si=si[sel,:];
+        if suf=='3':phi=phi[sel]
+    if suf=='3' and event>0: assert phi.shape[0]==si.shape[0]
     if plot:
         #plt.close('all')
         plt.figure()
         lim=12
         bn=np.linspace(-lim,lim,21)
-        bb,c,d=np.histogram2d(D[:,6],D[:,7],bins=[bn,bn])
+        bb,c,d=np.histogram2d(si[:,6],si[:,7],bins=[bn,bn])
         plt.imshow(bb, extent=[-lim,lim,-lim,lim],origin='lower',
                        interpolation='bicubic')#,vmin=-140,vmax=40)
         plt.colorbar()
@@ -71,14 +131,14 @@ def plotSearchInfo(plot=True):
         plt.title('Saccade Target Positions')
         
         plt.figure()
-        reloc=np.sqrt(np.sum(np.power(D[:,[6,7]]-D[:,[2,3]],2),axis=1))
+        reloc=np.sqrt(np.sum(np.power(si[:,[6,7]]-si[:,[2,3]],2),axis=1))
         plt.xlabel('Distance in Degrees')
         plt.hist(reloc,bins=np.linspace(0,20,41),normed=True)
         plt.xlim([0,20])
         plt.ylim([0,0.3])
 
         plt.figure()
-        dur = D[:,5]-D[:,1]
+        dur = si[:,5]-D[:,1]
         plt.hist(dur,bins=np.linspace(0,150,41),normed=True)
         plt.xlabel('Saccade Duration in ms')
         plt.xlim([0,150])
@@ -91,11 +151,12 @@ def plotSearchInfo(plot=True):
         plt.xlim([0,200])
         plt.ylim([0,0.035])
         #saveFigures('si')
-    return D,phi
+    if suf=='3': return si,phi
+    else: return si
 
 
-def extractTrajectories():
-    si,phi=plotSearchInfo(plot=False)
+def extractTrajectories(suf=''):
+    si,phi=plotSearchInfo(plot=False,suf=suf)
     inpath=os.getcwd().rstrip('code')+'input/' 
     sf=np.int32(np.round((si[:,1]+sw)/1000.0*hz))
     ef=sf+fw
@@ -105,8 +166,7 @@ def extractTrajectories():
     if not os.path.exists(path+'E%d'%(event)):
         os.makedirs(path+'E%d'%(event))
     np.save(path+'E%d/si.npy'%(event),si[valid])
-    if event>0:np.save(path+'E%d/phi2.npy'%(event),phi[valid])
-    return
+    if suf=='3':np.save(path+'E%d/phi%s.npy'%(event,suf),phi[valid])
     D=np.zeros((valid.sum(),fw,14,3))*np.nan
     DG=np.zeros((valid.sum(),fw,14,3))*np.nan
     DT=np.zeros((valid.sum(),fw,14,2))*np.nan
@@ -142,7 +202,7 @@ def extractTrajectories():
 ##                    DP[:,f,a,q]= D[:,f,a,q] - si[rp[k],6+q]
 ##        np.save(path+'E%d/DP%02d.npy'%(event,k),DP)
     D=[DG,DA,DT]       
-    for d in range(len(D)): np.save(path+'E%d/D%s.npy'%(event,dtos[d]),D[d])
+    for d in range(len(D)): np.save(path+'E%d/D%s%s.npy'%(event,dtos[d],suf),D[d])
 
 
 def extractDensity():
@@ -380,69 +440,13 @@ def plotTraj(traj):
 ##    y[:F[q].shape[0]]=1
 ##    del D, E, F
 
-##########################################
-#
-#       extract angles
-#
-##########################################
-
-def extractPhi(PLOT=False):
-    for vp in range(1,5):
-        initVP(vp)
-        f=open(path+'trdir.pickle','rb')
-        dt=pickle.load(f)
-        f.close()
-        #sds=[]
-        alp=0.05;clr='k'
-        phis=np.zeros((len(dt),35,2))*np.nan
-        for i in range(len(dt)):
-            tr=np.reshape(dt[i],(len(dt[i])/2,2))
-            tr-=tr[0]
-            phis[i,:min(34,tr.shape[0]),:]=tr[:34,:]
-            if PLOT:
-                plt.figure(1,figsize=(10,10));plt.subplot(2,2,vp)
-                plt.plot(tr[:34,0],tr[:34,1],'k',alpha=alp)
-                plt.grid(b=False)
-            temp=np.diff(tr,axis=0)
-            if PLOT:
-                plt.figure(2,figsize=(10,10));plt.subplot(2,2,vp)
-                plt.plot(np.linalg.norm(temp,axis=1),'k',alpha=alp)
-                plt.grid(b=False)
-            phi=np.arctan2(temp[:,1],temp[:,0])
-            
-            pold=np.copy(phi)
-            df=np.diff(phi)
-            df=-np.sign(df)*(np.abs(df)>np.pi)
-            phi[1:]+=np.cumsum(df)*2*np.pi
-            
-            if np.any(np.diff(phi)>np.pi):stop
-            phis[i,-1,0]=phi[:34].mean()
-            phis[i,-1,1]=np.median(phi)
-            phi-=phis[i,-1,0]
-            #sds.append(np.std(phi))
-            # is exclusion of outliers needed?
-            #if np.std(phi)>0.3: continue
-            if PLOT:
-                plt.figure(3,figsize=(10,10));plt.subplot(2,2,vp)
-                plt.plot(np.linspace(0,phi.size,phi.size),phi/np.pi*180,clr,alpha=alp)
-                plt.grid(b=False)
-                plt.ylim([-60,60])
-                c=np.cos(dd);s=np.sin(dd)
-                plt.figure(1,figsize=(10,10));plt.subplot(2,2,vp)
-                plt.plot([0,c],[0,s],'g',alpha=alp)
-                plt.gca().set_aspect(1)
-                plt.grid(b=False);plt.xlim([-1,1]);plt.ylim([-1,1])
-        np.save(path+'phiTrack',phis)
-        #print (np.array(sds)>0.3).mean()
-
 if __name__ == '__main__':
-    extractPhi()
-    for event in range(-6,0):
-    #for event in range(1,3):
+    #for event in range(-6,0):
+    for event in range(1,3):
         for vpl in range(1,5):
             initVP(vpl=vpl,evl=event)
             print vp, event
-            extractTrajectories()
+            extractTrajectories(suf='3')
             #extractDensity()
             #extractDirC()
             #extractSaliency(channel='COmotion')

code/PercFields.py
@@ -190,7 +190,7 @@ def PFparallel(vp,event,suf=''):
     ''' please run PFinit() first
     '''
     path,inpath,fp=initPath(vp,event)
-    E=np.load(inpath+'DG.npy')[:,:,:,:2]
+    E=np.load(inpath+'DG%s.npy'%suf)[:,:,:,:2]
     print E.shape
     stack=np.load(inpath+'stackPF.npy').tolist()
     f=open(inpath+'PF%s.pars'%suf,'r');dat=pickle.load(f);f.close()
@@ -212,11 +212,12 @@ def PFparallel(vp,event,suf=''):
                 print 'IOError'
                 core.wait(1)
     wind.close()
-##vp=4
-##PFinit(vp,2,suf='2')
-##PFparallel(vp,2,suf='2')
-##PFinit(vp,1,suf='2')
-##PFparallel(vp,1,suf='2')
+    
+vp=4
+#PFinit(vp,2,suf='3')
+#PFparallel(vp,2,suf='3')
+#PFinit(vp,1,suf='3')
+#PFparallel(vp,1,suf='3')
 
 #########################################################
 #                                                       #
@@ -509,7 +510,6 @@ def inithc(vp,event,s,suf=''):
     P=D.shape[1];F=D.shape[3]
     mask=createMask(P,F)
     D*=mask
-    np.save('test',D)
     weights=np.load(inpath+'svm%s/weights.npy'%suf)
     svs=np.load(inpath+'svm%s/svs.npy'%suf)
     svvs=np.zeros(D.shape[0])
@@ -570,7 +570,7 @@ def hcscript(vp,event,nworkers=8,s=2,suf=''):
         p.start();ps.append(p)
     for p in ps: p.join()
 
-hcscript(1,1,suf='2')
+#hcscript(2,1,suf='2')
 
 
 def svmPlotExtrema(event=0,plot=True):

code/readETData.py
@@ -479,8 +479,8 @@ def checkEyelinkDatasets():
         
 
 
-def saveSacInfo(vp=1):
-    ''' output gives,
+def saveETinfo(vp=1):
+    ''' si output gives,
             0- sac onset in f,
             1- sac onset in sec,
             2-sac onset posx,
@@ -494,7 +494,7 @@ def saveSacInfo(vp=1):
             10-event type of the consecutive event,
             11-start of tracking event in f,
             12-trial dur in sec,
-            13-tracking event id within trial,
+            13-sac id within tracking event counted backwards
             14-sac id within tracking event,
             15-block,
             16-trial
@@ -503,13 +503,14 @@ def saveSacInfo(vp=1):
     '''
     path=os.getcwd().rstrip('code')+'evaluation/vp%03d/'%vp
     import pickle
-    si=[];trackdir=[];finalev=[]
+    si=[];sacxy=[];finalev=[];ti=[];trackxy=[]
+    pi=[];purxy=[]
     for b in range(1,24):
         try: data=readEyelink(vp,b)
         except:
             print 'block %d is missing'%b
             continue
-        for i in range(1,len(data)):
+        for i in range(0,len(data)):
             if data[i].ts>=0:
                 print 'vp',vp,'block ',b,'trial',i
                 data[i].extractBasicEvents()
@@ -517,11 +518,11 @@ def saveSacInfo(vp=1):
                 data[i].importComplexEvents(coderid=4)
                 if  data[i].search!=None:
                     g=data[i].getGaze()
-                    hh=0
                     for ev in data[i].search:
                         si.append([ev[0],g[ev[0],0],g[ev[0],7],g[ev[0],8],
                             ev[1],g[ev[1],0],g[ev[1],7],g[ev[1],8],ev[2],ev[3],
-                            ev[4],np.nan,data[i].t0[1]-data[i].t0[0],hh,0,b,i]);hh+1
+                            ev[4],np.nan,data[i].t0[1]-data[i].t0[0],0,0,b,i])
+                        sacxy.append(g[ev[0]:ev[1],[7,8]].flatten().tolist())
                     gg=0
                     for tr in data[i].track:
                         if False:#len(tr[4:])>0:
@@ -529,13 +530,20 @@ def saveSacInfo(vp=1):
                             # i.e. tracking started with a blink or directly with pursuit 
                             print '\tdif', g[tr[0],0],g[tr[4][1],0],tr[0]-tr[4][1]
                         kk=1
-                        for ev in tr[4:]:
+                        for ev in tr[4]:
                             si.append([ev[0],g[ev[0],0],g[ev[0],7],g[ev[0],8],
                                 ev[1],g[ev[1],0],g[ev[1],7],g[ev[1],8],ev[2],ev[3],
-                                ev[4],tr[0],data[i].t0[1]-data[i].t0[0],gg,kk,b,i]); kk+=1
-                            trackdir.append(g[ev[0]:ev[1],[7,8]].flatten().tolist())
+                                ev[4],tr[0],data[i].t0[1]-data[i].t0[0],
+                                kk-len(tr[4])-1,kk,b,i]);
+                            sacxy.append(g[ev[0]:ev[1],[7,8]].flatten().tolist())
+                            if len(ev)>5:
+                                pi.append([b,i,kk-len(tr[4])-1,kk,
+                                           ev[-1][0],g[ev[-1][0],0],ev[-1][1],g[ev[-1][1],0]])
+                                purxy.append(g[ev[-1][0]:ev[-1][1],[7,8]].flatten().tolist())
+                            kk+=1
                         gg+=1
                     if data[i].msgs=='DETECTION':
+                        si[-1][13]=1
                         res=[[b,i,0]]
                         for ttt in [-251,-201,-151,-101,-51,-26]:
                             if data[i].isFix[ttt] or data[i].opur[ttt] or data[i].cpur[ttt]:
@@ -544,35 +552,47 @@ def saveSacInfo(vp=1):
                         finalev.append(res)  
                     elif data[i].msgs!='OMISSION':raise ValueError('Unexpected message')
     np.save(path+'finalev',finalev)
-    f=open(path+'trdir.pickle','wb')
-    pickle.dump(trackdir,f)
-    f.close()
-    np.save(path+'sinew.npy',si)
+    f=open(path+'purxy.pickle','wb')
+    pickle.dump(purxy,f);f.close()
+    f=open(path+'sacxy.pickle','wb')
+    pickle.dump(sacxy,f);f.close()
+    np.save(path+'si.npy',si)
+    np.save(path+'pi.npy',pi)
+
+def saveTrackingInfo(vp):
+    path=os.getcwd().rstrip('code')+'evaluation/vp%03d/'%vp
+    si=np.load(path+'si.npy')
+    temp=np.int32(si[:,[15,16,0]])
+    indices=np.lexsort((temp[:,2],temp[:,1],temp[:,0]))
+    si=si[indices,:].tolist();ti=[];trackxy=[]
+    import pickle
+    for b in range(1,24):
+        try: data=readEyelink(vp,b)
+        except:
+            print 'block %d is missing'%b
+            continue
+        for i in range(0,len(data)):
+            if data[i].ts>=0:
+                print 'vp',vp,'block ',b,'trial',i
+                data[i].extractBasicEvents()
+                data[i].driftCorrection()
+                g=data[i].getGaze()
+                while len(si) and si[0][-2]==b and si[0][-1]==i:
+                    if len(ti) and len(ti[-1])==6:
+                        ti[-1].extend([si[0][0],si[0][1]])
+                        trackxy.append(g[ti[-1][4]:ti[-1][6],[7,8]].flatten().tolist())
+                    if si[0][-3]>0: ti.append(si[0][13:]+[si[0][4],si[0][5]])
+                    out=si.pop(0)
+                if len(ti) and len(ti[-1])==6:
+                    ti[-1].extend([g.shape[0],g[-1,0]])
+                    trackxy.append(g[ti[-1][4]:ti[-1][6],[7,8]].flatten().tolist())
+    np.save(path+'ti.npy',ti)
+    f=open(path+'trackxy.pickle','wb')
+    pickle.dump(trackxy,f);f.close()
+                
+    
     
     
 if __name__ == '__main__':
-##    data=readEyelink(1,1)
-##    data[1].extractBasicEvents()
-##    data[1].driftCorrection()
-##    data[1].importComplexEvents()
-    saveSacInfo(vp=4)
-    #sacInfoMergeBlocks(vp=1)
-    
-            
-##    res=np.zeros(2549)
-##    for b in range(1,22):
-##        data=readEyelink(1,b)
-##        for i in range(40):
-##            data[i].extractBasicEvents()
-##            data[i].loadTrajectories()
-##            D=data[i].oldtraj
-##            print i, D.shape
-##            for a in range(14):
-##                res+=np.logical_or(np.abs(np.diff(D[:,a,1],2))>0.0001,
-##                        np.abs(np.diff(D[:,a,0],2))>0.0001)
-##            
-
-        
-##    data[1].extractBasicEvents()
-##    data[1].driftCorrection()
-##    data[1].importComplexEvents()
+    saveETinfo(vp=1)
+    saveTrackingInfo(vp=1)