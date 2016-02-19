fc741fa5b83a659557b9af82b21198965f95cca3
matus <matus@desktop>
2016-02-19 20:08:42 +0100

    sklearn version

--- code/ClassifyTrackSearch.py
+++ code/ClassifyTrackSearch.py
@@ -1,51 +1,53 @@
 #from ipyparallel import Client
-#import numpy as np
 #rc = Client(profile='chase')
 #dview= rc[:]
 
 import pickle
-def svmTrain(args,fn='svm',kernelm=0,subsample=0.01,ncpu=8):
+def svmTrain(args,fn='svm',kernelm=0,subsample=0.5,ncpu=8,ncv=5):
     data,kernelvar,penalty=args
     from commands import getstatusoutput
     import numpy as np
+    from sklearn.svm import SVC
     #remove nans
     data=data[np.isnan(data).sum(1)==0,:]
     X=data[:,1:];y=data[:,0];N=X.shape[0]
     #whitening
     X-=np.array(X.mean(0),ndmin=2)
     X/=2*np.array(X.std(0),ndmin=2)
+    # randomize
+    inds=np.random.permutation(N)
+    X=X[inds,:];y=y[inds]
+    # subsample
     if subsample<1:
-        r=np.random.permutation(N)
         N=int(N*subsample)
-        sel=r[:N]
-        X=X[sel,:]; y=y[sel]
-    print 'export'
+        X=X[:N,:]; y=y[:N]
+    print 'compute kernel'
     kv=np.round(kernelvar,3)
-    f=open(fn+'%.3f.in'%kv,'w')
+    S=np.zeros((N,N),order='C')
     for row in range(N):
         D=np.atleast_2d(X[row,:])-X
         D=np.square(D).sum(1)
-        s='%d 0:%d'%(int(y[row]),row+1)
-        S=np.exp(-np.exp(kernelvar)*(D-kernelm))
-        for col in range(N):
-            s+=' %d:%.6f'%(col+1,S[col])
-        s+='\n'
-        f.write(s)
-    f.close()
-    del D,S
+        S[row,:]=np.exp(-np.exp(kernelvar)*(D-kernelm))
+    del D
     print 'train','kernelvar=',kv
-    out=''
-    for p in penalty:
-        # assure that worker has svm-train compiled with 
-        out+='kernelvar=%.1f\npenalty=%.1f\n'%(kv,p)
-        s,o=getstatusoutput('export OMP_NUM_THREADS=%d'%ncpu+' && svm-train -s'+
-            ' 0 -v 5 -t 4 -c %f -m 12000 %s%.3f.in'%(np.exp(p),fn,kv))
-        if s: out+=o+'\nCross Validation Accuracy = 0%\n'
-        else: out+=o+'\n' 
-    getstatusoutput('rm '+fn+'%.3f.in'%np.round(kernelvar,3))
-    print 'finished' 
+    out=[]
+    for r in range(ncv):
+        spl=int(N-N/float(ncv))
+        S1=np.copy(S[:spl,:spl]);S2=np.copy(S[spl:,:spl])
+        for p in penalty:
+            if not r: out.append([kernelvar,p])
+            
+
+            svm=SVC(C=np.exp(p),cache_size=12000,verbose=True,kernel='precomputed')
+            svm.fit(S1,y[:spl])
+            res=svm.predict(S2)
+            acc=np.abs(res-y[spl:]).mean()
+            out[-1].append(acc)
+        print acc
+        bla
     return out
 if __name__ == '__main__':
+    import numpy as np
     kernelvars=np.linspace(-10,10,21)
     penalty=np.linspace(-10,10,21)
     for vp in range(1,5):
@@ -60,8 +62,5 @@ if __name__ == '__main__':
         d=d[:,1:]
         for kv in kernelvars:
             fargs.append([d,kv,penalty])
-        out=dview.map(svmTrain,fargs)    
-        f=open(path+'svm.log','w')
-        for ot in out: f.write(ot)
-        f.close()
+        out=map(svmTrain,fargs) 
 
